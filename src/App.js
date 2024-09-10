87ebcd550b4fed3c4d6fe3e9f48a9cf41223ae99
Saran2304 <sarans2304@gmail.com>
2024-09-10 17:37:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/portfolio/Home'
-import Card from './card/Card';
 
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      {/* <Card></Card> */}
+      
       <Routes>
 
       </Routes>
